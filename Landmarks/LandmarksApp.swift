d4004178907e509de2a16bf56fe0e9db864c45b2
mhasan341 <mhasan341@gmail.com>
2022-01-03 15:08:34 +0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Mahmudul Hasan on 2022-01-03.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
